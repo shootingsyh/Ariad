d15f6eaf8e6c7c8a1e590bc422588fb4b8e587ce
shootingsyh <shootingsyh@gmail.com>
2026-09-16 11:16:00 -0700

    feat: add runtime registry

---
+++ src/runtime-registry.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { validateRuntimeAdapter } = require('./runtime-adapter');
+
+class RuntimeRegistry {
+  constructor() {
+    this.adapters = new Map();
+  }
+
+  register(key, adapter, options = {}) {
+    if (typeof key !== 'string' || key.trim() === '') {
+      throw new Error('runtime key must be a non-empty string');
+    }
+    validateRuntimeAdapter(adapter);
+    if (this.adapters.has(key) && !options.replace) {
+      throw new Error(`runtime ${key} already registered`);
+    }
+    this.adapters.set(key, adapter);
+    return adapter;
+  }
+
+  get(key) {
+    if (!this.adapters.has(key)) {
+      throw new Error(`unknown runtime: ${key}`);
+    }
+    return this.adapters.get(key);
+  }
+
+  has(key) {
+    return this.adapters.has(key);
+  }
+
+  list() {
+    return [...this.adapters.entries()].map(([key, adapter]) => ({ key, id: adapter.id }));
+  }
+}
+
+module.exports = { RuntimeRegistry };
